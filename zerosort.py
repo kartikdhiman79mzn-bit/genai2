bb3a849a6cdde3221d070be0ff700d1c7c48b1e3
karitkdhiman79mzn-bit <kartikdhiman79mzn@gmail.com>
2026-06-07 12:24:52 +0530

    Add zerosort.py for math-related AI assistant functionality

---
+++ zerosort.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+     
+     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+response = client.chat.completions.create(
+    model='gemini-3.5-flash',
+    messages=[
+        { 'role': 'system', 'content': 'You are helpful AI assistant which is master to solve math related problem only and only. If user ask any question which is not realted to math then say "Sorry I am not able to answer this."' },
+        { 'role': 'user', 'content': 'What is 2 + 2?' }
+    ]
+)
+
+print(response.choices[0].message.content)
